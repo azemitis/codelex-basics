74201b92c0f7e0d3b8dfcda7750926ee77ac7c7b
azemitis <7-days@inbox.lv>
2023-03-21 14:46:43 +0200

    lizard&spock

--- rock-paper-scissors/rock-paper-scissors.php
+++ rock-paper-scissors/rock-paper-scissors.php
@@ -2,21 +2,27 @@
 
 /**  Rock-paper-scissors game in PHP */
 
-$choices = ['rock', 'paper', 'scissors'];
-$computerChoice = $choices[rand(0, 2)];
+$choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+$computerChoice = $choices[rand(0, 4)];
 
-$userInput = readline('Select rock, paper, or scissors: ');
+$userInput = readline('Select rock, paper, scissors, lizard, or spock: ');
 
-while (!in_array($userInput, $choices)) {
-    echo "Invalid input. Select rock, paper, or scissors: ";
+while (!in_array($userInput, $choices))
+{
+    echo "Invalid input. Select rock, paper, scissors, lizard, or spock: ";
     $userInput = readline();
 }
 
-if ($userInput == $computerChoice) {
-    echo "It's a tie!";
-} elseif (($userInput == 'rock' && $computerChoice == 'scissors') ||
-    ($userInput == 'paper' && $computerChoice == 'rock') ||
-    ($userInput == 'scissors' && $computerChoice == 'paper')) {
+if ($userInput == $computerChoice)
+{
+    echo "It's a draw!";
+} elseif (
+    ($userInput == 'rock' && ($computerChoice == 'scissors' || $computerChoice == 'lizard')) ||
+    ($userInput == 'paper' && ($computerChoice == 'rock' || $computerChoice == 'spock')) ||
+    ($userInput == 'scissors' && ($computerChoice == 'paper' || $computerChoice == 'lizard')) ||
+    ($userInput == 'lizard' && ($computerChoice == 'paper' || $computerChoice == 'spock')) ||
+    ($userInput == 'spock' && ($computerChoice == 'rock' || $computerChoice == 'scissors'))
+) {
     echo "You win! Computer chose $computerChoice";
 } else {
     echo "Computer wins! Computer chose $computerChoice";
